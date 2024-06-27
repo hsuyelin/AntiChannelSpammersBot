97ab21d754785a7d59cef3109a8f646d6d3c402a
xuyanling <xuyanling@huami.com>
2024-06-27 15:30:30 +0800

    fix: webhook stat error get

--- index.js
+++ index.js
@@ -13,35 +13,44 @@ const throttler = telegrafThrottler();
 const { token, admin, webhookUrl, webhookPort } = process.env;
 const bot = new Telegraf(token);
 
-log('开始启动...');
-Data.load();
+const startBot = async () => {
+    log('开始启动...');
+    Data.load();
 
-bot.on('message', (ctx) => handleMessage(ctx));
-bot.on('callback_query', (ctx) => handleCallbackQuery(ctx));
-bot.catch((err, ctx) => log(err.stack, true, ctx.message || ctx.callbackQuery));
-bot.use(throttler);
+    bot.on('message', (ctx) => handleMessage(ctx));
+    bot.on('callback_query', (ctx) => handleCallbackQuery(ctx));
+    bot.catch((err, ctx) => log(err.stack, true, ctx.message || ctx.callbackQuery));
+    bot.use(throttler);
 
-if (webhookUrl) {
-    const app = express();
-    const url = new URL(webhookUrl);
-    app.use(await bot.createWebhook({ domain: url.origin, path: url.pathname }));
-    app.get('stats', (req, res) => {
-        res.json({
-            'schemaVersion': 1,
-            'label': '使用中群组',
-            'message': Analytics.activeGroupsCount().toString() + ' 个',
-            'color': '#26A5E4',
-            'namedLogo': 'Telegram',
-            'style': 'flat'
+    if (webhookUrl) {
+        const app = express();
+        const url = new URL(webhookUrl);
+        const webhookMiddleware = await bot.createWebhook({ domain: url.origin, path: url.pathname });
+        app.use(webhookMiddleware);
+
+        app.get('/stats', (req, res) => {
+            res.json({
+                'schemaVersion': 1,
+                'label': '使用中群组',
+                'message': Analytics.activeGroupsCount().toString() + ' 个',
+                'color': '#26A5E4',
+                'namedLogo': 'Telegram',
+                'style': 'flat'
+            });
         });
-    });
-    app.listen(webhookPort, () => log(`已设置 WebHook, Express 服务器正在监听 ${webhookPort} 端口`));
-}
-else {
-    log('没有设置 Webhook，开始轮询');
-    await bot.launch();
-}
-const botName = (await bot.telegram.getMe()).username;
-log(`${botName} 启动完成`);
+
+        app.listen(webhookPort, () => log(`已设置 WebHook, Express 服务器正在监听 ${webhookPort} 端口`));
+    } else {
+        log('没有设置 Webhook，开始轮询');
+        await bot.launch();
+    }
+
+    const botName = (await bot.telegram.getMe()).username;
+    log(`${botName} 启动完成`);
+};
+
+startBot().catch(error => {
+    log(`启动 Bot 时发生错误: ${error.stack}`, true);
+});
 
 export { bot, token, admin };
